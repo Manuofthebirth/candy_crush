0d6f7af227e40ea5ef1e2b164c2e8de9ab0412c2
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-01 15:50:17 -0300

    square random colors added to grid

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', () =>{
-  const grid = document.querySelector('.game-grid')
-  const width = 8
-  const squares = []
+  const grid = document.querySelector('.game-grid');
+  const width = 8;
+  const squares = [];
+
+  const candyColors = [
+    'red',
+    'yellow',
+    'orange',
+    'purple',
+    'green',
+    'blue'
+  ]
 
   // create Board 
   function createBoard() {
     for (let i = 0; i < width*width; i++) {
       const square = document.createElement('div')
-      square.classList.add('square')
-      grid.appendChild(square)
-      squares.push(square)
+      square.classList.add('square');
+      let randomCandy = Math.floor(Math.random() * candyColors.length);
+      square.style.backgroundColor = candyColors[randomCandy]
+      grid.appendChild(square);
+      squares.push(square);
     }
   }
   createBoard()

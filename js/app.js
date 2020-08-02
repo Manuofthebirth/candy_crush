ee5d4752c58dcfcd661beb4069207db3fddb280b
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-01 21:44:02 -0300

    game board completed

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () =>{
     for (let i = 0; i < width*width; i++) {
       const square = document.createElement('div')
       square.classList.add('square');
+      square.setAttribute('draggable', true);
+      square.setAttribute('id', i); // each square gets an unique id from 0 to 63
       let randomCandy = Math.floor(Math.random() * candyColors.length);
       square.style.backgroundColor = candyColors[randomCandy]
       grid.appendChild(square);
